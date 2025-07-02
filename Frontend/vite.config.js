602340be3037f7f84b8520594a31ba89189de397
Jairo221451 <221451@unsaac.edu.pe>
2025-07-02 02:04:04 -0500

    Resturacion del FRontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.REACT_APP_BACKEND_URL': '"https://proyecto-production-5103.up.railway.app/api"'
+  },
   server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    host: '0.0.0.0', // Permite conexiones externas
+    port: 5173
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173
+    port: 5173
   }
 })
