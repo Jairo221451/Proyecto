0201ae88db8d9c5d396fbd6bf65f0c0756fd7e59
Jairo221451 <221451@unsaac.edu.pe>
2025-07-02 02:12:27 -0500

    Probando cambios

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     'process.env.REACT_APP_BACKEND_URL': '"https://proyecto-production-5103.up.railway.app/api"'
   },
   server: {
-    host: '0.0.0.0', // Permite conexiones externas
+    host: '0.0.0.0',
     port: 5173
   },
   preview: {
     host: '0.0.0.0',
-    port: 5173
+    port: process.env.PORT || 5173  // Usar el puerto que Railway asigne
   }
 })
